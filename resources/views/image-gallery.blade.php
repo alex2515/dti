3701e5d222320809901620b96931e4f05a983390
Alexander E <alexander-310@hotmail.com>
2018-10-07 13:19:33 -0500

    URl galery

--- resources/views/image-gallery.blade.php
+++ resources/views/image-gallery.blade.php
@@ -75,8 +75,8 @@
                                 @if($images->count())
                                     @foreach($images as $image)
                                     <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                                        <a class="thumbnail fancybox" rel="ligthbox" href="/images/{{ $image->image }}">
-                                            <img class="img-responsive" alt="" src="/images/{{ $image->image }}" />
+                                        <a class="thumbnail fancybox" rel="ligthbox" href="asset('/images/{{ $image->image }}')">
+                                            <img class="img-responsive" alt="" src="asset('/images/{{ $image->image }}')" />
                                             <div class='text-center'>
                                                 <small class='text-muted'>{{ $image->title }}</small>
                                             </div> <!-- text-center / end -->
